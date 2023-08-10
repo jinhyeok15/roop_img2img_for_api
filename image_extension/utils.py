726aac3911203ffa5afb284dc325ad8711afb6a8
jinhyeok15 <ekfrhd97@gmail.com>
2023-08-10 23:18:07 +0900

    base64 string 전처리 추가

--- image_extension/utils.py
+++ image_extension/utils.py
@@ -2,6 +2,8 @@ import base64
 import numpy as np
 
 def get_buffer_image(base64img: str):
+    if base64img.startswith("data:image/"):
+        base64img = base64img.split(";")[1].split(",")[1]
     image_data = base64.b64decode(base64img)
     nparr = np.frombuffer(image_data, np.uint8)
 
