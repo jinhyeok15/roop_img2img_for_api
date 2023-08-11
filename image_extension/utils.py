564c5b398ccb428daef37baf2b0bc3a54cdb56ee
jinhyeok15 <ekfrhd97@gmail.com>
2023-08-11 21:29:23 +0900

    buffer 전환시 jpg로 return하도록 수정

--- image_extension/utils.py
+++ image_extension/utils.py
@@ -1,5 +1,9 @@
 import base64
 import numpy as np
+import cv2
+
+class ConversionError(Exception):
+    pass
 
 def get_buffer_image(base64img: str):
     if base64img.startswith("data:image/"):
@@ -7,7 +11,13 @@ def get_buffer_image(base64img: str):
     image_data = base64.b64decode(base64img)
     nparr = np.frombuffer(image_data, np.uint8)
 
-    return nparr
+    image = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+
+    # 이미지를 JPG 형식으로 인코딩하여 메모리 버퍼에 저장
+    success, jpg_buffer = cv2.imencode(".jpg", image)
+    if (success):
+        return jpg_buffer
+    raise ConversionError("Failed to encode image as JPG.")
 
 
 def get_base64_from_buffer(buffer):
